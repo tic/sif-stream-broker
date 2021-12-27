0c45f89b014d0776a23aeea3650e696f4ff7a4ea
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2021-12-27 17:32:56 -0500

    stream incoming messages into timescale db

---
+++ db.py
@@ -0,0 +1,93 @@
+# Import libraries
+import psycopg2
+from psycopg2 import sql
+import json
+
+
+# Inserts an Intermediate Representation (IR)
+# formatted message into the database.
+def insert_ir_message(db_connection, app_id, ir_message):
+    # Incoming message is a parsed IR-formed json
+
+    # There must be payload in the incoming message
+    payload_length = len(ir_message['payload'])
+    assert(payload_length > 0)
+
+    # Extract top-level elements
+    app_id = sql.SQL('{}').format(sql.Identifier(app_id)).as_string(db_connection)
+    unix_timestamp = ir_message['time']
+
+    # Aggregate columns and values
+    # md_specifiers = ', %s'*len(ir_message['metadata'])
+    # md_specifiers = (', ' if len(ir_message['metadata']) > 0 else '') + ', '.join([f'{{{md_field}}}' for md_field in ir_message['metadata']])
+    md_specifiers = ', {}'*len(ir_message['metadata'])
+
+    columns_as_identifiers = [sql.Identifier(column) for column in ir_message['metadata'].keys()]
+    column_template = sql.SQL('(time, metric, value' + md_specifiers + ')') \
+                         .format(*columns_as_identifiers) \
+                         .as_string(db_connection)
+
+    md_value_specifiers = ', %s'*len(ir_message['metadata'])
+    # Create value templates. Each row has at least time,
+    # metric, and value columns. Additionally, there may
+    # be a variable number of metadata columns.
+    value_template = '(to_timestamp(%s), %s, %s' + md_value_specifiers + ')'
+
+    
+    # Stores parameters to fill each %s in the eventual insertion string
+    parameters = []
+
+
+    # Add metadata column names to the insert statement
+    metadata_values = [value for value in ir_message['metadata'].values()]
+
+
+    # Add payload data to the insert statement
+    for metric, value in ir_message['payload'].items():
+        point_values = [
+            unix_timestamp,
+            metric,
+            value
+        ]
+        point_values.extend(metadata_values)
+        parameters.extend(point_values)
+
+
+    # Construct template insert string
+    # NOTE: While format strings are generally vulnerable to SQL
+    #       injection attacks, its usage here is believed to be 
+    #       SAFE for two reasons:
+    #         a) it originates from the ingest-broker, which has
+    #            logic to safe all app ids that it sees;
+    #         b) the app_id variable is fed through a SQL identifier
+    #            prior to the usage of a format string here
+    query_template = f'INSERT INTO {app_id} ' \
+        + column_template \
+        + ' VALUES ' \
+        + value_template \
+        + (', ' + value_template)*(payload_length - 1) \
+        + ';'
+    print(query_template)
+    print(parameters)
+
+
+    cursor = db_connection.cursor()
+    cursor.execute(query_template, tuple(parameters))
+    db_connection.commit()
+    cursor.close()
+
+
+# Attempts to create a db connection object
+# and returns the connection or False if
+# unsuccessful.
+def create_connection(ts_user, ts_passwd, ts_host, ts_port):
+    try:
+        # Template format string for the connection URL
+        connection_url = f'postgres://{ts_user}:{ts_passwd}@{ts_host}:{ts_port}/tsdb?sslmode=require'
+
+        # Connect to the db and store the connection object
+        connection = psycopg2.connect(connection_url)
+        return connection
+    except Exception as err:
+        print(err)
+        return False
---
+++ sif_stream_broker.py
@@ -0,0 +1,95 @@
+# Import necessary libraries
+import paho.mqtt.client as mqtt
+from datetime import datetime
+import json
+
+# Import database connector and create a blank
+# connection object
+import db
+db_connection = None
+
+# Load env variables and the function to read them
+from os import getenv as os_getenv
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Executes when a message arrives in the ingest stream
+def on_message_receive(client, userdata, message):
+    message = message.payload.decode("utf-8")
+    parsed = json.loads(message)
+    print(message)
+    db.insert_ir_message(db_connection, parsed['app_id'], parsed['data'])
+
+
+# Create a mqtt client with a unique time-based name.
+# Note: uniqueness is guaranteed provided multiple
+#       clients are not created at the exact same
+#       instance in time.
+def spawn_client():
+    # Create a client instance with a unique name
+    ctime = datetime.utcnow().timestamp()
+    return mqtt.Client(f'StreamProcessor.{ctime}')    
+
+
+# Given a client, configure it for receiving messages
+# on the ingest stream. After this function returns,
+# the specified client will be elligible to receive
+# messages from the ingest stream.
+def setup_client(client):
+    # Connect to the ingest stream host
+    if os_getenv('ENV') == 'DEVELOPMENT':
+        client.connect('localhost')
+    else:
+        client.connect(os_getenv('INGEST_STREAM'))
+
+    # Initiate the client's event loop
+    client.loop_start()
+
+    # Subscribe to the ingest data stream
+    client.subscribe('ingest/stream')
+
+    # Register the on_message handler
+    client.on_message = on_message_receive
+
+
+# Properly shuts down a client instance by ending its
+# event loop and disconnecting it from the ingest
+# stream.
+def cleanup_client(client):
+    client.loop_stop()
+    client.disconnect()
+
+
+# Generate a single client and wait. After the loop
+# is stopped by a user, shut down the client and
+# terminate.
+if __name__ == '__main__':
+    # Import libraries
+    from time import sleep
+
+    # Spawn a single consumer client
+    client = spawn_client()
+
+    db_connection = db.create_connection(
+        os_getenv('TS_USER'), 
+        os_getenv('TS_PASSWD'), 
+        os_getenv('TS_HOST'), 
+        os_getenv('TS_PORT')
+    )
+
+    if not db_connection:
+        raise 'Failed to connect to database'
+
+    # Prepare the client to receive messages
+    setup_client(client)
+
+    # Loop indefinitely until stopped by a user
+    try:
+        while True:
+            sleep(60)
+    except KeyboardException:
+        pass
+
+    # Clean up the client before shutting down
+    cleanup_client(client)
