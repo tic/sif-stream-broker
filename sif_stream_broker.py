e749b821a43baf9faca6d9121bebf3f61dfe18b8
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2022-01-31 12:09:00 -0500

    add channel arg

--- sif_stream_broker.py
+++ sif_stream_broker.py
@@ -16,9 +16,9 @@ load_dotenv()
 
 # Executes when a message arrives in the ingest stream
 def on_message_receive(client, userdata, message):
+    print('message received')
     message = message.payload.decode("utf-8")
     parsed = json.loads(message)
-    print(message)
     db.insert_ir_message(db_connection, parsed['app_id'], parsed['data'])
 
 
@@ -36,18 +36,18 @@ def spawn_client():
 # on the ingest stream. After this function returns,
 # the specified client will be elligible to receive
 # messages from the ingest stream.
-def setup_client(client):
+def setup_client(client, channel):
     # Connect to the ingest stream host
     if os_getenv('ENV') == 'DEVELOPMENT':
-        client.connect('localhost')
+        client.connect('localhost', keepalive = 30)
     else:
-        client.connect(os_getenv('INGEST_STREAM'))
+        client.connect(os_getenv('INGEST_STREAM'), keepalive = 30)
 
     # Initiate the client's event loop
     client.loop_start()
 
     # Subscribe to the ingest data stream
-    client.subscribe('ingest/stream')
+    client.subscribe('ingest/stream/' + channel)
 
     # Register the on_message handler
     client.on_message = on_message_receive
@@ -65,6 +65,13 @@ def cleanup_client(client):
 # is stopped by a user, shut down the client and
 # terminate.
 if __name__ == '__main__':
+    # Get channel from argv
+    from sys import argv
+    if len(argv) != 2:
+        raise Exception("Bad command line arguments. Provide a channel argument and nothing else.")
+    channel = argv[1]
+    print('using channel', channel)
+
     # Import libraries
     from time import sleep
 
@@ -83,7 +90,7 @@ if __name__ == '__main__':
         raise 'Failed to connect to database'
 
     # Prepare the client to receive messages
-    setup_client(client)
+    setup_client(client, channel)
 
     # Loop indefinitely until stopped by a user
     try:
